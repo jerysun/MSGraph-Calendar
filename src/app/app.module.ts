f19cbfb90e04d4743a8371305405ca1685160bcd
Jerry Sun <jerysun007@hotmail.com>
2021-10-25 19:53:17 +0200

    Get calendar view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 let msalInstance: IPublicClientApplication | undefined = undefined;
 
@@ -39,6 +40,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
     NavBarComponent,
     HomeComponent,
     AlertsComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
